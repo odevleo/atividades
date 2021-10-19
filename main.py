08adf7364c9e73cdf9b0f5afe1e1ae035cd821ce
Leonardo <leobragab@hotmail.com>
2021-10-19 18:39:31 -0300

    Desafio 1 e 3

---
+++ main.py
@@ -0,0 +1,18 @@
+""" Desafio 01 - Programa para executar algumas perguntas para o usuário informar."""
+from Pessoa import *
+
+def info():
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    sexo = input("Digite seu sexo: ")
+    cidade = input("Digite sua cidade: ")
+    estado = input("Digite seu Estado: ")
+    return nome, idade, sexo, cidade, estado
+
+
+print(info())
+
+pessoa = Pessoa(input("Digite seu nome: "), int(input("Digite sua idade: " )), input("Digite seu sexo: "), 
+input("Digite sua cidade: "), input("Digite seu Estado: ")) 
+
+print(pessoa.Nome)
