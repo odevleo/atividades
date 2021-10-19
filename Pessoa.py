825c340bbf3d95f8475ed53a16e4d4353bcf44cb
Leonardo <leobragab@hotmail.com>
2021-10-19 18:39:15 -0300

    Desafio 2

---
+++ Pessoa.py
@@ -0,0 +1,11 @@
+class Pessoa: 
+    def __init__(self, nome, idade, sexo, cidade, estado):
+        self.Nome = nome
+        self.Idade = idade
+        self.Sexo = sexo
+        self.Cidade = cidade
+        self.Estado = estado
+
+
+pessoa = Pessoa('Leonardo', '25', 'M', 'Fortaleza', 'Ceará')
+
